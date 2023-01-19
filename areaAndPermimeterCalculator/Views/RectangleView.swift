d3135d3e80576a32bb46d90f1f0f361bbc5455ab
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-19 11:17:03 -0500

    Set up an empty view to calculate the area and perimeter of a rectangle

---
+++ areaAndPermimeterCalculator/Views/RectangleView.swift
@@ -0,0 +1,20 @@
+//
+//  RectangleView.swift
+//  areaAndPermimeterCalculator
+//
+//  Created by Cooper Lawson on 2023-01-19.
+//
+
+import SwiftUI
+
+struct RectangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleView()
+    }
+}
