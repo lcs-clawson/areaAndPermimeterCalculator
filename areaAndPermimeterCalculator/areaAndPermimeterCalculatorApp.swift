771c4bbdfb5c61d3f76a6278738997b48d2c0c76
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-19 11:05:38 -0500

    Initial Commit

---
+++ areaAndPermimeterCalculator/areaAndPermimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  areaAndPermimeterCalculatorApp.swift
+//  areaAndPermimeterCalculator
+//
+//  Created by Cooper Lawson on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct areaAndPermimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
